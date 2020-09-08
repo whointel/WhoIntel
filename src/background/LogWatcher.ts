33f1f548cb105f33ef90a0e4901aaacf74814660
Dmitry Shulgachik <intel@whointel.space>
2020-09-08 15:54:39 +0300

    LogWatcher: restart file change watcher instead of interval polling (optimize on large file number)

--- src/background/LogWatcher.ts
+++ src/background/LogWatcher.ts
@@ -5,13 +5,17 @@ import * as log from "electron-log"
 import * as normalizePath from "normalize-path"
 import differenceInHours from "date-fns/differenceInHours"
 import Timeout = NodeJS.Timeout
+import {Subscription, of} from "rxjs"
+import {delay, repeat, tap} from "rxjs/operators"
+
+const FILES_POLLING_INTERVAL = 300
 
 export default class LogWatcher extends EventEmitter {
 	private dirWatcher: fs.FSWatcher | null = null
 	private files = new Set()
 	private filesWatched = new Map()
-	private readonly filesPoolingInterval: Timeout
 	private readonly filesPoolingForCleaningInterval: Timeout
+	private filesPoolingSubscription$: Subscription | null = null
 
 	constructor(dir: string) {
 		super()
@@ -19,9 +23,18 @@ export default class LogWatcher extends EventEmitter {
 		if (!startResult) {
 			throw new Error(`${dir} is not a dir`)
 		}
-		this.filesPoolingInterval = setInterval(this.filesPooling.bind(this), 300)
+
+		const filesPooling$ = of({}).pipe(
+			tap(_ => {
+				this.filesPooling()
+			}),
+			delay(FILES_POLLING_INTERVAL),
+			repeat()
+		)
+
+		setTimeout(_ => this.filesPoolingSubscription$ = filesPooling$.subscribe(), FILES_POLLING_INTERVAL)
+
 		this.filesPoolingForCleaningInterval = setInterval(this.filesPoolingForCleaning.bind(this), 1000 * 60 * 60)
-		setTimeout(this.filesPoolingForCleaning.bind(this), 5000)
 	}
 
 	private watchDir(dir: string): boolean {
@@ -64,12 +77,20 @@ export default class LogWatcher extends EventEmitter {
 
 		setTimeout(() => {
 			const files = fs.readdirSync(dir)
+			const nowDate = new Date()
 			files.forEach(filename => {
 				const filepath = normalizePath(dir + path.sep + filename)
 				const stats = fs.statSync(filepath)
 				if (!stats.isFile()) {
 					return
 				}
+
+				const diffH = differenceInHours(nowDate, stats.mtime)
+				if (diffH > 24) {
+					log.info(`LogWatcher:init:ignoring:diffH=${diffH}:`, filepath)
+					return
+				}
+
 				this.files.add(filepath)
 
 				this.emit("add", filepath, stats)
@@ -119,15 +140,17 @@ export default class LogWatcher extends EventEmitter {
 		if (this.filesWatched.delete(filepath)) {
 			this.emit("unwatch", filepath)
 		}
+		this.files.delete(filepath)
 	}
 
 	close() {
-		clearInterval(this.filesPoolingInterval)
+		if (this.filesPoolingSubscription$) this.filesPoolingSubscription$.unsubscribe()
 		clearInterval(this.filesPoolingForCleaningInterval)
 		if (this.dirWatcher) {
 			this.dirWatcher.close()
 			this.dirWatcher = null
 		}
 		this.filesWatched.forEach((stats, filepath) => this.unwatch(filepath))
+		this.files.clear()
 	}
 }
