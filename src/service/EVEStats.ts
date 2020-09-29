b0cba457029c93c254bf72742dc2dec0a47718f2
Dmitry Shulgachik <intel@whointel.space>
2020-09-29 20:41:38 +0300

    refactoring

--- src/service/EVEStats.ts
+++ src/service/EVEStats.ts
@@ -2,8 +2,13 @@ import api, {apiPoll} from "@/lib/EVEApi"
 import store from "@/store"
 import systemManager from "@/service/SystemManager"
 import events from "@/service/EventBus"
+import {Subscription} from "rxjs"
 
 class EVEStats {
+	status$ = new Subscription()
+	kills$ = new Subscription()
+	jumps$ = new Subscription()
+
 	constructor() {
 		events.$on("systemManager:ready", async () => {
 			await this.requestKills()
@@ -16,8 +21,9 @@ class EVEStats {
 	}
 
 	requestStatus() {
-		const status$ = apiPoll(api.status$(), {name: "status"})
-		status$.subscribe(
+		this.status$.unsubscribe()
+
+		this.status$ = apiPoll(api.status$(), {name: "status"}).subscribe(
 			(data) => {
 				store.commit("setEVEStatus", data)
 			},
@@ -25,8 +31,9 @@ class EVEStats {
 	}
 
 	requestKills() {
-		const kills$ = apiPoll(api.system_kills$(), {name: "system_kills"})
-		kills$.subscribe(
+		this.kills$.unsubscribe()
+
+		this.kills$ = apiPoll(api.system_kills$(), {name: "system_kills"}).subscribe(
 			(kills) => {
 				if (!kills) return
 				kills.forEach(record => systemManager.setSystemKills(record))
@@ -35,8 +42,9 @@ class EVEStats {
 	}
 
 	requestJumps() {
-		const jumps$ = apiPoll(api.system_jumps$(), {name: "system_jumps"})
-		jumps$.subscribe(
+		this.jumps$.unsubscribe()
+
+		this.jumps$ = apiPoll(api.system_jumps$(), {name: "system_jumps"}).subscribe(
 			(jumps) => {
 				if (!jumps) return
 				jumps.forEach(record => systemManager.setSystemJumps(record))
